a5aadd76e3f60b58e9d134910b09b31a076dd658
CMax <max.loktev.52@googlemail.com>
2025-09-26 17:17:26 +0200

    Initial commit

---
+++ src/main/java/com/CMax/swRedstoneLimit/SwRedstoneLimit.java
@@ -0,0 +1,10 @@
+package com.CMax.swRedstoneLimit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SwRedstoneLimit implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
